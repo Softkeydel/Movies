981a091bf0da456ad256d6a080c9cd666a1991bc
Softkeydel <softkeydel@gmail.com>
2023-11-30 00:04:38 +0530

    dev:DashboardFragment added with ui design

---
+++ app/src/main/java/com/imdb/movies/view/dashboard/DashboardFragment.kt
@@ -0,0 +1,43 @@
+package com.imdb.movies.view.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.imdb.movies.databinding.FragmentDashboardBinding
+
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class DashboardFragment : Fragment() {
+
+    private var _binding: FragmentDashboardBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
